7bab325444ef775082c8a512c0c8bf4bdfb2b9f9
Michael Delgado <madelgado@sbgtv.com>
2020-04-06 23:50:00 -0700

    Updates

---
+++ precache-manifest.2d50474a9757aae88a6572f1d9910a59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43e2ef1248a8a5d98574e890a0ecead9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "469715aa869e7c4c11bc",
+    "url": "/static/css/main.927b625f.chunk.css"
+  },
+  {
+    "revision": "88b926bbced3f23cd623",
+    "url": "/static/js/2.aa1afa21.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.aa1afa21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "469715aa869e7c4c11bc",
+    "url": "/static/js/main.b3786a7b.chunk.js"
+  },
+  {
+    "revision": "fbc43e371532511c1f0d",
+    "url": "/static/js/runtime-main.5af94b4c.js"
+  }
+]);
