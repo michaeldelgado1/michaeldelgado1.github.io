90ac80254f6e36fe33f384bd2863b8b53a5ffd41
Michael Delgado <madelgado@sbgtv.com>
2020-04-07 13:49:03 -0700

    Adding a nav, and only options. I can now test things in a more hidden fashon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import './App.css';
+import Nav from './components/nav'
+
+import {
+  // BrowserRouter as Router,
+  HashRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
@@ -7,6 +15,25 @@ function App() {
       <header className="App-header">
         Welcome, this site is under construction.
       </header>
+
+      <Router>
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/test">
+            <Nav />
+            Test Page
+          </Route>
+          <Route path="/users">
+          <Nav />
+            Users Page
+          </Route>
+          <Route path="/">
+          </Route>
+        </Switch>
+      </div>
+    </Router>
     </div>
   );
 }
