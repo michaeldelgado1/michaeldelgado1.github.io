e086e22f903af7735fe130520201e2c50177dbd1
Michael Delgado <madelgado@sbgtv.com>
2020-04-06 23:19:11 -0700

    Giving a little more info

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Welcome
+        Welcome, this site is under construction.
       </header>
     </div>
   );
